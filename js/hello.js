82faeadd0157254eee0bea21c8c883b10925aee8
bidulgirin <de05164@naver.com>
2022-03-23 12:49:56 +0900

    모듈을 defalut 로 export 할 시 어떤 조건으로 해야하는지..

--- js/hello.js
+++ js/hello.js
@@ -1,7 +1,7 @@
 
 //전역객체 쓰지 말랬는데 일단 예제로 이러는 거겠지머... 내보낼함수라면 function 앞에 export 쓰면 됨
-
-export function hello1(){
+//export 할때 default 를 쓰면 {}를 사용할수없음
+export default function hello1(){
     console.log('hello.js!!!');
 }
 export function hello2(){
--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 // export 한걸 가져오려면 import 
 //import { hello1 , hello2 } from './hello.js'  // hello1 함수에 {}를 치면 번거롭지 않게.. 아 import 하니까 ...응...
-import * as hello from "./hello.js"
+//import * as hell from "./hello.js"
+//hell 이런거 같은경우 우리가 임의로 정한 이름이다...hello 든hell 이든 저 경로("./hello.js")만 맞으면 상관없이 쓸수있다.....!!!!
+import hello1 from './hello.js';
+hello1(); 
+hell.hello2();  
 
-hello.hello1(); 
-hello.hello2(); 
+
