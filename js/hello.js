93dc817311136ae1242e2e7dd30b45fd402d8ae8
bidulgirin <de05164@naver.com>
2022-03-23 12:40:10 +0900

    first commit

---
+++ js/hello.js
@@ -0,0 +1,9 @@
+
+//전역객체 쓰지 말랬는데 일단 예제로 이러는 거겠지머... 내보낼함수라면 function 앞에 export 쓰면 됨
+
+export function hello1(){
+    console.log('hello.js!!!');
+}
+export function hello2(){
+    console.log('heloo! hellloooo');
+}
---
+++ js/main.js
@@ -0,0 +1,6 @@
+// export 한걸 가져오려면 import 
+//import { hello1 , hello2 } from './hello.js'  // hello1 함수에 {}를 치면 번거롭지 않게.. 아 import 하니까 ...응...
+import * as hello from "./hello.js"
+
+hello.hello1(); 
+hello.hello2(); 
