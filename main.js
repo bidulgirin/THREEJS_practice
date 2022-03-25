e8f82a1e077cf29d782d8371f8edaddb9eefb992
bidulgirin <de05164@naver.com>
2022-03-25 13:50:57 +0900

    카메라 설정 하고 설치함

--- main.js
+++ main.js
@@ -6,4 +6,9 @@ import * as THREE from 'three';
 
 const canvas =cocument.querySelector('#three-canvas');
 const renderer = new HTMLPreElement.WebGLRenderer({canvas}); //canvas:canvas
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(window.innerWidth, window.innerHeight); 
+
+const scene = new THREE.Scene(); //장면 가져오기 
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z= 5;
+scene.add( camera );
