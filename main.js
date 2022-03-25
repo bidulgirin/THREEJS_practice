679df390d56c115030de03a30852da0aa3752760
bidulgirin <de05164@naver.com>
2022-03-25 13:27:27 +0900

    renderer 셋팅 완료

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 import * as THREE from 'three';
 
-const renderer = new THREE.WebGLRenderer();
+// const renderer = new THREE.WebGLRenderer();
+// renderer.setSize(window.innerWidth, window.innerHeight);
+// document.body.appendChild(renderer.domElement); // render.domElement는 canvas와 같다
+
+const canvas =cocument.querySelector('#three-canvas');
+const renderer = new HTMLPreElement.WebGLRenderer({canvas}); //canvas:canvas
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement); // render.domElement는 canvas와 같다
