497656b8c4ab76b1fd8b101fa0879b3caa2767d0
bidulgirin <de05164@naver.com>
2022-04-02 15:18:20 +0900

    orthographicCamera

--- main.js
+++ main.js
@@ -11,22 +11,42 @@ const renderer = new THREE.WebGLRenderer({
 }); //canvas:canvas
 renderer.setSize(window.innerWidth, window.innerHeight); 
 
-const scene = new THREE.Scene(); //장면 가져오기 
-const camera = new THREE.PerspectiveCamera(
-75, //시야각 
-window.innerWidth / window.innerHeight, //종횡비(aspect)
- 0.1, // near
- 1000 //far
- );
+const scene = new THREE.Scene(); //장면 가져오기
 //camera
+// const camera = new THREE.PerspectiveCamera(
+// 75, //시야각 
+// window.innerWidth / window.innerHeight, //종횡비(aspect)
+//  0.1, // near
+//  1000 //far
+//  );
+
+// camera.position.x = 1;
+// camera.position.y = 2;
+// camera.position.z = 5;
+
+
+//Orthographic Camera (직교카메라) 롤같은 게임에 나오는 카메라
+
+const camera = new THREE.OrthographicCamera(
+    -(window.innerWidth / window.innerHeight),//left 값
+    window.innerWidth / window.innerHeight, //right 값
+    1, //top
+    -1, // bottom
+    0.1,
+    1000
+)
+
 camera.position.x = 1;
 camera.position.y = 2;
 camera.position.z = 5;
+camera.lookAt(0,0,0);
+camera.zoom = 0.5;
+camera.updateProjectionMatrix(); // 카메라 설정을 바꿧으면 업뎃해줘야한다...
 scene.add( camera ); //씬에 카메라를 넣는다
 //  Mesh 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({
-    color: 'red'
+    color: 'pink'
 });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh); //씬에 mesh를 넣는다 
