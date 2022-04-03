5c75a85106d46695f1a5dd3948088a9374887333
bidulgirin <de05164@naver.com>
2022-04-03 14:55:23 +0900

    scene에다가 배경화면 색상 넣는 방법

---
+++ ex03-1.js
@@ -0,0 +1,57 @@
+import * as THREE from 'three';
+
+// 주제: 배경색 투명도 설정하기
+
+
+export default function example(){
+// Renderer
+const canvas = document.querySelector('#three-canvas');
+const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true,
+    alpha: true // 바탕화면이 투명해짐
+}); 
+renderer.setSize( window.innerWidth, window.innerHeight); 
+renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1); 
+//renderer.setClearAlpha(0.5); 
+//renderer.setClearColor(0X00ff00); 
+
+//Scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color('blue'); // scene 설정된 background 색상은 renderer것을 덮어 버린다
+//투명도 조절은 안됨 
+//Camera
+const camera = new THREE.PerspectiveCamera(
+ 75, 
+ window.innerWidth / window.innerHeight, 
+ 0.1, 
+ 1000 
+ );
+
+camera.position.set(1,2,5);
+camera.lookAt(0,0,0);
+camera.zoom = 0.5;
+camera.updateProjectionMatrix();
+scene.add( camera ); 
+//  Mesh 
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({
+    color: 'pink'
+});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+renderer.render(scene, camera); 
+
+// resize event
+function setSize(){
+    camera.aspect = window.innerWidth / window.innerHeight; 
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+}
+
+window.addEventListener( 'resize' , setSize);
+
+}
+
+
