c7d7fe641363dd8061d44b02361f824babc9495c
bidulgirin <de05164@naver.com>
2022-06-29 19:30:17 +0900

    ex02

--- three_js_Raycaster_study/src/ex02.js
+++ three_js_Raycaster_study/src/ex02.js
@@ -78,21 +78,38 @@ export default function example() {
 		renderer.setSize(window.innerWidth, window.innerHeight);
 		renderer.render(scene, camera);
 	}
-	
+	function checkIntersects(){
+		raycaster.setFromCamera(mouse, camera); 
+		/* origin을 카메라에 있다고 생각하셈,
+			raycaster를 camera시점 기준으로 mouse에 클릭한 지점에다가 광선을 셋팅하는것임
+		*/
+		const intersects = raycaster.intersectObjects(meshes); 
+		for (const item of intersects){ //forEach 대신에 쓴것임
+			item.object.color.set('red');
+			break; // break로 첫번째 관통된 물체만 선택,  break를 안하면 for of문이 배열을 돌면서 관통한 물체들을 전부 선택함
+			
+			// if (intersects[0]){ --> intersect의 첫번째 배열이 있다면 그것의 이름만 출력하여라, 라고 해야 오류안뜸
+			//	console.log(intersect[0].object.name) }이런식으로 사용해도 됨 ...쓰기 나름임 
+			
+		}
+
+	}
 	// 이벤트
 	window.addEventListener('resize', setSize);
 	canvas.addEventListener('click', e =>{
 
 		console.log(e.clientX , e.clientY); 
 		/* 
+			--마우스 위치 정규화 하기
 			마우스로 클릭한 위치를 return 함 왼쪽위모서리가 기준임, 
 			하지만 threeJs에서는 맨가운데가 기준이므로 설정을 맞춰줘야한다!
 			three.js기준으로 clientX,Y값으로 맞춰준것임
 		
 		*/
+		
 		mouse.x = e.clientX / canvas.clientWidth * 2 - 1; // 식 설명: 클릭한위치/ 캔버스크기 => 비율이 나옴 비율에 두배 해주고 절반값인 1을 빼주면 중간 값이 나옴
 		mouse.y = -(e.clientY / canvas.clientHeight * 2 - 1); // threejs에서는 위쪽이 y가 +이므로 -을붙혀 값을 맞춰준다 걍 외우자~~~
-		console.log(mouse); 
+		checkIntersects();
 	});
 	draw();
 }
